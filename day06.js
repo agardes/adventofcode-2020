6d5082677606c5df99b442bd741798c630e4c50a
Anne Gardes <anne.gardes@hotmail.com>
2020-12-06 07:48:46 +0100

    day06

--- day06.js
+++ day06.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
 const read = fs.readFileSync("day06.txt");
 let data = read.toString().split("\n\n").map(el=>el.split('\n'))
-let c = 0
-let c2 = 0
-data.forEach((el) => {
+let day06 = data.map((el)=>{
     let l = []
     let pTwo = {}
-    let p = el.length
+    let c = 0
     el.forEach((el2)=>{
         for(let k=0;k<el2.length;k++){
             pTwo[el2[k]] ? pTwo[el2[k]]++ : pTwo[el2[k]] = 1
             l.indexOf(el2[k])==-1 ? l.push(el2[k]) : undefined
         }
     })
-    c+=l.length
     for (const [key, value] of Object.entries(pTwo)) { 
-        value==p ? c2++ : undefined
-    } 
-})
-console.log('Part one = ' +c)
-console.log('Part two = ' +c2)
+        value==el.length ? c++ : undefined
+    }
+    return [l.length,c]
+}).reduce((a,b)=>[a[0]+b[0],a[1]+b[1]])
+
+console.log(`Part one = ${day06[0]} - Part two = ${day06[1]}`)
+
