6bffb471a0d8c9548d33075d569c64b8637c6931
Anne Gardes <anne.gardes@hotmail.com>
2020-12-17 08:32:40 +0100

    day17

--- day17.js
+++ day17.js
@@ -2,76 +2,63 @@ const fs = require('fs');
 const read = fs.readFileSync("day17.txt");
 let data = read.toString().split('\r\n')
 let actives = []
-console.log(data)
 
-let minZ = minY = minX = minW = -1
-let maxZ = maxW = 1
-let maxX = data[0].length
-let maxY = data.length
 data.forEach((line,y)=>{
     line = line.split('')
-    line.forEach((cube,x)=>{
-        if(cube=='#'){
-            actives.push(x+'|'+y+'|'+0)
-        }
-    })
+    line.forEach((cube,x)=>{ cube=='#' ? actives.push(x+'|'+y+'|'+0+'|'+0) : undefined })
 })
+let reset = [...actives]
 
-console.log(actives)
-let oldActives= [...actives]
-let cycle = 0
-while(cycle<6){
-    cycle++
-    let newActives = []
-    for(let y=minY;y<=maxY;y++){
-        for(let x=minX;x<=maxX;x++){
-            for(let z=minZ;z<=maxZ;z++){
-                    let coord = x+'|'+y+'|'+z
-                    let nei = getNei(x,y,z)
-                    // if it's active
-                    if(oldActives.indexOf(coord)!==-1){
-                        let intersection = oldActives.filter(x => nei.includes(x));
-                        if(intersection.length==3 || intersection.length==2 ){
-                            newActives.push(coord)
-                        }
-                    }else{
-                        let intersection = oldActives.filter(x => nei.includes(x));
-                        if(intersection.length==3){
-                            newActives.push(coord)
+const day17 = {
+    run(part){
+        let oldActives= [...reset]
+        let cycle = 0
+        let minZ = minY = minX = -1
+        minW = part=='one' ? 0 : -1
+        let maxZ = 1
+        maxW = part=='one' ? 0 : 1
+        let maxX = data[0].length
+        let maxY = data.length
+        while(cycle<6){
+            cycle++
+            let newActives = []
+            for(let y=minY;y<=maxY;y++){
+                for(let x=minX;x<=maxX;x++){
+                    for(let z=minZ;z<=maxZ;z++){
+                        for(let w=minW;w<=maxW;w++){
+                            let coord = x+'|'+y+'|'+z+'|'+w
+                            let nei = getNei(x,y,z,w)
+                            let intersection = oldActives.filter(x => nei.includes(x));
+                            oldActives.indexOf(coord)!==-1 ? intersection.length==3 || intersection.length==2 ? newActives.push(coord) : undefined 
+                            : intersection.length==3 ? newActives.push(coord) : undefined 
                         }
                     }
+                
+                } 
             }
-        
-        } 
+            oldActives = newActives
+            part!== 'one' ? (minW -= 1,maxW += 1) :undefined
+            minZ -= 1, minY -= 1, minX -= 1, maxZ += 1, maxX += 1, maxY += 1
+            cycle== 6 ? console.log('Part '+part+' = ' + newActives.length) : undefined
+        }
+
+
     }
-    oldActives = newActives
-    minZ -= 1
-    minY -= 1
-    minX -= 1
-    maxZ += 1
-    maxX += 1
-    maxY += 1
-    if(cycle==6){
-        console.log('Part one ' + newActives.length)
-    }
-    // console.log('////// Cycle ' + cycle)
-    // console.log(newActives)
 }
 
-function getNei(x,y,z){
+function getNei(x,y,z,w){
     let arr = []
     for(let xX=-1;xX<=1;xX++){
         for(let yY=-1;yY<=1;yY++){
             for(let zZ=-1;zZ<=1;zZ++){
-                if(x+xX==x && y+yY==y && z+zZ==z){
-
-                }else{
-                    arr.push((x+xX)+'|'+(y+yY)+'|'+(z+zZ)) 
-                }
-                    
+                for(let wW=-1;wW<=1;wW++){
+                    x+xX==x && y+yY==y && z+zZ==z && w+wW==w ? undefined : arr.push((x+xX)+'|'+(y+yY)+'|'+(z+zZ)+'|'+(w+wW)) 
+                }   
             }
         }
     }
     return arr
 }
 
+day17.run('one')
+day17.run('two')
